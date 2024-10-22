dc8b874efb5b5a819c2ec6edfec7b348f1cf482c
Mariana Naselli <nasellimariana@outlook.com>
2024-10-22 22:43:04 +0200

    Here's a summary of the changes:
    🛠️ src/app/layout/header/header.component.html -> Updated site name from "MiAmorcito" to "Mariana Naselli".
    🔴 src/app/layout/header/header.component.html -> Removed multiple navigation items including "Services" and dropdown menus.
    🛠️ src/app/layout/main/main.component.html -> Refactored structure to use custom section components for better organization.
    🛠️ src/app/layout/main/main.component.ts -> Imported multiple section components into the main component.

---
+++ src/app/sections/resume/resume.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'section-resume',
+  standalone: true,
+  imports: [],
+  templateUrl: './resume.component.html',
+  styleUrl: './resume.component.css'
+})
+export class ResumeComponent {
+
+}
