e1a6b8787e0cd37c9b443df5031c014d71a49dd1
Mariana Naselli <nasellimariana@outlook.com>
2024-10-23 19:19:37 +0200

    🛠️ package-lock.json -> Added "typed.js" dependency 🛠️ package.json -> Included "typed.js" in dependencies 🛠️ hero.component.html -> Improved HTML structure 🛠️ hero.component.ts -> Integrated Typed.js for typing effect 🔴 about.json -> Removed redundant Photoshop entries

--- src/app/components/sections/hero/hero.component.ts
+++ src/app/components/sections/hero/hero.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild, Inject, PLATFORM_ID } from '@angular/core';
+import Typed from 'typed.js';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'section-hero',
   standalone: true,
-  imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrls: ['./hero.component.css']
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit {
+  @ViewChild('typedElement') typedElement!: ElementRef;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const typedStrings = this.typedElement.nativeElement.getAttribute('data-typed-items');
+      const options = {
+        strings: typedStrings.split(','),
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+      };
+      
+      new Typed(this.typedElement.nativeElement, options);
+    }
+  }
 }
