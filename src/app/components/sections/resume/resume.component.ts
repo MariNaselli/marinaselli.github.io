6999868cb04c395ccc11c0da58fcfded7a1082aa
Mariana Naselli <nasellimariana@outlook.com>
2024-10-24 23:42:38 +0200

    🛠️ src/app/components/sections/resume/resume.component.html -> Updated static content to dynamic data. 🛠️ src/app/components/sections/resume/resume.component.ts -> Added OnInit interface; fetched dynamic resumeData.

--- src/app/components/sections/resume/resume.component.ts
+++ src/app/components/sections/resume/resume.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ResumeService } from './services/resume.service';
 
 @Component({
   selector: 'section-resume',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
-export class ResumeComponent {
+export class ResumeComponent implements OnInit{
 
+  resumeData: any;
+
+  constructor(private resumeService: ResumeService){}
+
+  ngOnInit(): void {
+    // Llamar al servicio para obtener los datos
+    this.resumeService.getResumeData().subscribe((data) => {
+      this.resumeData = data.resume;
+    });
+  }
 }
