d69ad74409ce4073f10de0e733041bea09899c4e
Mariana Naselli <nasellimariana@outlook.com>
2024-10-28 16:34:45 +0100

    🛠️ src/app/components/sections/contact/contact.component.html -> Updated content to use dynamic data. 🛠️ src/app/components/sections/contact/contact.component.ts -> Added data fetching logic for contact info.

--- src/app/components/sections/contact/contact.component.ts
+++ src/app/components/sections/contact/contact.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from './services/contact.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'section-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+
+  contactData: any = null;
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    // Llamar al servicio para obtener los datos
+    this.contactService.getContactData().subscribe((data) => {
+      this.contactData = data.contact;
+    });
+  }
 
 }
