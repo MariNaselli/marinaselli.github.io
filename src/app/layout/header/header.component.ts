ca5e04733383744dbac45fc3d50b891be6d5f457
Mariana Naselli <nasellimariana@outlook.com>
2024-11-07 19:06:30 +0100

    🛠️ src/app/layout/header/header.component.html -> Added toggle method for menu items.
    🛠️ src/app/layout/header/header.component.ts -> Imported CommonModule and added menu toggle logic.
    
    🛠️ src/assets/css/main.css -> Changed navmenu position to sticky.

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,18 @@
-import { ViewportScroller } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'layout-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+
+  menuOpen = false;
+  
   constructor() {}
 
   scrollToSection(sectionId: string): void {
@@ -21,5 +24,12 @@ export class HeaderComponent {
         element.getBoundingClientRect().top + window.scrollY - offset;
       window.scrollTo({ top: yPosition, behavior: 'smooth' });
     }
+  }
+
+  toggleBodyClass() {
+    // Verifica si el body tiene la clase `mobile-nav-active` y la alterna
+    document.body.classList.toggle('mobile-nav-active');
+
+    this.menuOpen = !this.menuOpen;
   }
 }
