0f43d308c0d448cd9ad22f42032a12b4ba59c8cb
Mariana Naselli <nasellimariana@outlook.com>
2024-11-07 17:09:13 +0100

    🛠️ src/app/layout/header/header.component.ts -> Refactored scroll logic 🛠️ src/app/layout/main/main.component.ts -> Modified scroll behavior on init

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,25 +7,19 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  
-  constructor(private viewportScroller: ViewportScroller) {}
+  constructor() {}
 
   scrollToSection(sectionId: string): void {
-    // Ajuste de desplazamiento adicional solo para "portfolio" y "about"
-    if (sectionId === 'portfolio' || sectionId === 'about') {
-      const offset = 100; // Ajusta el valor según el tamaño de tu header
-      const element = document.getElementById(sectionId);
-      
-      if (element) {
-        const yPosition = element.getBoundingClientRect().top + window.scrollY - offset;
-        window.scrollTo({ top: yPosition, behavior: 'smooth' });
-      }
-    } else {
-      // Desplazamiento normal para las demás secciones
-      this.viewportScroller.scrollToAnchor(sectionId);
+    const offset = 100; // Ajusta el valor según el tamaño de tu header
+    const element = document.getElementById(sectionId);
+
+    if (element) {
+      const yPosition =
+        element.getBoundingClientRect().top + window.scrollY - offset;
+      window.scrollTo({ top: yPosition, behavior: 'smooth' });
     }
   }
 }
